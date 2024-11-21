cad37c5a7bf37fe352ce79603f1dbf44cacda03a
Andrei Neacșu <0x514@protonmail.com>
2024-11-21 12:40:38 +0200

    add missing colon

--- mac/vlc-protocol.m
+++ mac/vlc-protocol.m
@@ -10,24 +10,47 @@
   // Get input data
   NSString *input = [[event paramDescriptorForKeyword:keyDirectObject] stringValue];
   NSString *url;
+  NSString *urlTemp;
+
+  NSLog(@"Received input: %@", input);
+
   if ([input hasPrefix:@"vlc://"]) {
-    url = [input substringFromIndex:6];
+    urlTemp = [input substringFromIndex:6];
+    NSLog(@"URL after vlc:// prefix: %@", urlTemp);
   }
   else if ([input hasPrefix:@"vlc:"]) {
-    url = [input substringFromIndex:4];
+    urlTemp = [input substringFromIndex:4];
+    NSLog(@"URL after vlc: prefix: %@", urlTemp);
   }
   else {
     // invalid input
+    NSLog(@"Invalid input, terminating application.");
     [NSApp terminate:nil];
     return;
+  }
+
+  if ([urlTemp hasPrefix:@"http//"]) {
+    // Replace "http//" with "http://"
+    url = [urlTemp stringByReplacingCharactersInRange:NSMakeRange(0, 6) withString:@"http://"];
+  }
+  else if ([urlTemp hasPrefix:@"https//"]) {
+    // Replace "https//" with "https://"
+    url = [urlTemp stringByReplacingCharactersInRange:NSMakeRange(0, 7) withString:@"https://"];
+  }
+  else {
+    // No changes if it doesn't start with "https//"
+    url = urlTemp;
   }
 
   // Only allow urls starting with http:// or https://
   if (![url hasPrefix:@"http://"] && ![url hasPrefix:@"https://"]) {
     // protocol not allowed
+    NSLog(@"URL protocol not allowed, terminating application: %@", url);
     [NSApp terminate:nil];
     return;
   }
+
+  NSLog(@"Valid URL: %@", url);
 
   // Launch VLC
   NSWorkspace *ws = [NSWorkspace sharedWorkspace];
